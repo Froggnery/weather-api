3b30566f744b05c76a58a618e6272d6dac74902c
Froggnery <nermata0127@gmail.com>
2025-10-20 14:25:09 +0800

    data fetch

--- resources/js/pages/Weather/weatherPage.tsx
+++ resources/js/pages/Weather/weatherPage.tsx
@@ -1,5 +1,7 @@
 import { weather } from '@/routes';
 import { type BreadcrumbItem } from '@/types';
+import axios from 'axios';
+import { useState } from 'react';
 
 const breadcrumbs: BreadcrumbItem[] = [
     {
@@ -9,20 +11,36 @@ const breadcrumbs: BreadcrumbItem[] = [
 ];
 
 export default function Weather() {
+    const [data, setData] = useState();
+    const [location, setlocation] = useState('');
+    const searchLocation = (event: { key: string }) => {
+        if (event.key === 'Enter') {
+            axios.get(url).then((response) => {
+                setData(response.data);
+                console.log(response.data);
+            });
+            setlocation("");
+        }
+    };
+    const API_KEY = 'd0e2a10085bc9611e4ac4c6b4ad60aa9';
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${API_KEY}&units=metric`;
+
     return (
-        <div className="flex h-full flex-1 flex-col gap-4 overflow-x-auto rounded-xl p-4">
-            <div>
-                <header className="mb-4">
-                    <h1 className="text-2xl font-bold">
-                        Welcome to Weather API
-                    </h1>
-                    <p className="text-muted-foreground">
-                        Get started by exploring the features and capabilities
-                        of the Weather API.
-                    </p>
-                </header>
+        <div className="relative h-full w-full bg-gradient-to-b from-blue-300 to-white py-2">
+            <div className="p-4 text-center">
+                <input
+                    type="text"
+                    className="bg-white-600/100 w-[700px] rounded-3xl border-1 px-6 py-3 text-lg shadow-md placeholder:text-gray-400 focus:outline-none"
+                    placeholder="Enter location"
+                    value={location}
+                    onChange={(e) => setlocation(e.target.value)}
+                    onKeyDownCapture={searchLocation}
+                />
             </div>
-            <div className="grid auto-rows-min gap-4 md:grid-cols-3"></div>
+             <div className='w-[500px] h-[500px] border-2 rounded-lg a bg-white shadow-lg mx-auto mt-10 relattive'> 
+     
+            </div>
         </div>
+        
     );
 }
